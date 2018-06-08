dbe07b0438f4251c39bc93fb970a01dea866116e
Denis ROZIMOVSCHII <rozimovschii.denis@gmail.com>
2018-06-08 16:53:37 +0300

    Updated book controller example

--- app/controllers/book_controller.py
+++ app/controllers/book_controller.py
@@ -5,9 +5,23 @@ import json
 
 @Routing.Route(url='/book/{book_id::\d+}', method='GET')
 def get_book(variables={}, request={}):
+    status = 'success'
+    message = ''
+    values = None
+
+    if not 'token' in request.params:
+        status = 'error'
+        message = 'not authorized'
+    else:
+        message = 'book returned'
+        values = {
+            'token' : request.params['token'][0],
+            'book_id' : variables['book_id']
+        }
+
     result = {
-        'status' : 'Success',
-        'book_id' : variables['book_id'],
-        'limit' : variables['limit']
+        'status' : status,
+        'message' : message,
+        'result' : values
     }
     return Controller.response_data(json.dumps(result), response_mimetype='text/json')
